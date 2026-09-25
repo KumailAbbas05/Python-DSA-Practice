f8a0fe1308a99c42e0a3b4d8380edac5aaa4e5d0
Kumail Abbas <generalpowers8@gmail.com>
2026-09-25 20:44:53 +0500

    Add array insertion exercise

---
+++ arrays/insertion.py
@@ -0,0 +1,10 @@
+numbers = [20, 30, 40]
+
+numbers.insert(0, 10)
+print("After beginning insertion:", numbers)
+
+numbers.insert(2, 25)
+print("After middle insertion:", numbers)
+
+numbers.append(50)
+print("After end insertion:", numbers)
