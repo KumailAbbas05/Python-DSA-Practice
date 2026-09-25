bc058e7511bff2b530b457736f5166149918601b
Kumail Abbas <generalpowers8@gmail.com>
2026-09-25 20:44:55 +0500

    Add array deletion exercise

---
+++ arrays/deletion.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40, 50]
+
+numbers.pop(0)
+print("After deleting first element:", numbers)
+
+numbers.pop(1)
+print("After deleting middle element:", numbers)
+
+numbers.pop()
+print("After deleting last element:", numbers)
